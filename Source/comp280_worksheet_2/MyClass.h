061c8ea83a6301f30c54ab990793f227c22524e4
Ashley Sands <ashsands1@amstrike.com>
2019-12-20 15:35:04 +0000

    Converted project from Blueprints to c++

---
+++ Source/comp280_worksheet_2/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COMP280_WORKSHEET_2_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
