498b9c688f1160b0372d73ffd698fffe8b9a023e
Ashley Sands <ashsands1@amstrike.com>
2019-12-23 02:46:28 +0000

    added gameInstance

---
+++ Source/comp280_worksheet_2/Pacman_gameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Pacman_gameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMP280_WORKSHEET_2_API UPacman_gameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
