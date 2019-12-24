9d526c385305d9d89685844b745e7bef3861676a
Ashley Sands <ashsands1@amstrike.com>
2019-12-24 01:34:27 +0000

    added http request helper class

---
+++ Source/comp280_worksheet_2/HTTP_Request.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HTTP_Request.h"
+
+HTTP_Request::HTTP_Request()
+{
+}
+
+HTTP_Request::~HTTP_Request()
+{
+}
---
+++ Source/comp280_worksheet_2/HTTP_Request.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COMP280_WORKSHEET_2_API HTTP_Request
+{
+public:
+	HTTP_Request();
+	~HTTP_Request();
+};
