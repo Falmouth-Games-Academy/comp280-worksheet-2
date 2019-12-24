8e9ad735657dc33b80842182d4f1af7c9abdd999
Ashley Sands <ashsands1@amstrike.com>
2019-12-24 01:36:02 +0000

    added http_request helper function ( forgot to hit save :D )

--- Source/comp280_worksheet_2/HTTP_Request.cpp
+++ Source/comp280_worksheet_2/HTTP_Request.cpp
@@ -5,8 +5,55 @@
 
 HTTP_Request::HTTP_Request()
 {
+	http = &FHttpModule::Get();
 }
 
-HTTP_Request::~HTTP_Request()
+HTTP_Request::HTTP_Request(FString base_url) : HTTP_Request::HTTP_Request()
 {
+
+	baseUrl = base_url;
+
+}
+
+HTTP_Request::~HTTP_Request() {}
+
+TSharedRef<IHttpRequest> HTTP_Request::GetRequestHeaders(FString p_request)
+{
+
+	if (http == NULL) http = &FHttpModule::Get();
+
+	TSharedRef<IHttpRequest> request = http->CreateRequest();
+	request->SetURL(baseUrl + "/" + p_request);
+	// Set headers
+	request->SetHeader(TEXT("User-Agent"), TEXT("X-UnrealEngine-Agent"));
+	request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	request->SetHeader(TEXT("Accepts"), TEXT("application/json"));
+
+	return request;
+}
+
+TSharedRef<IHttpRequest> HTTP_Request::GET_Request(FString page_request)
+{
+
+	TSharedRef<IHttpRequest> request = GetRequestHeaders(page_request);
+	request->SetVerb("GET");
+
+	return request;
+
+}
+
+TSharedRef<IHttpRequest> HTTP_Request::POST_Request(FString page_request, FString jsonStr)
+{
+
+	TSharedRef<IHttpRequest> request = GetRequestHeaders(page_request);
+	request->SetVerb("POST");
+	request->SetContentAsString(jsonStr);
+
+	return request;
+
+}
+
+void HTTP_Request::SendRequest(TSharedRef<IHttpRequest> request)
+{
+	request->ProcessRequest();
 }
--- Source/comp280_worksheet_2/HTTP_Request.h
+++ Source/comp280_worksheet_2/HTTP_Request.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Runtime/Online/HTTP/Public/Http.h"
 
 /**
  * 
@@ -11,5 +12,20 @@ class COMP280_WORKSHEET_2_API HTTP_Request
 {
 public:
 	HTTP_Request();
+	HTTP_Request(FString base_url);
 	~HTTP_Request();
+
+private:
+
+	FHttpModule* http;
+	FString baseUrl = "127.0.0.1:8000";
+
+	/// @Param Request: the page to request.
+	TSharedRef<IHttpRequest> GetRequestHeaders(FString p_request);
+
+public:
+	TSharedRef<IHttpRequest> GET_Request(FString page_request);		// use to recive data from db
+	TSharedRef<IHttpRequest> POST_Request(FString page_request, FString jsonStr);				// use to send data to db
+
+	void SendRequest(TSharedRef<IHttpRequest> request);
 };
